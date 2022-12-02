92f70c02ea84392d70d3891d9e43c7eb8017e52b
OLGA DRACH <dolchik88@mail.ru>
2022-12-02 22:52:42 +0300

    Программа выводит четные числа от 1 до N

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Ввеите число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 2;
+
+if (N > 0)
+{
+    while (count <= N)
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    }
+}
+
