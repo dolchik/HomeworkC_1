6ef4e5ef6910fe51dbba9cfce478502c72d8f07f
OLGA DRACH <dolchik88@mail.ru>
2022-12-07 21:04:42 +0300

    Обновление кода с переменной max

--- Zadacha_4/Program.cs
+++ Zadacha_4/Program.cs
@@ -3,6 +3,7 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+
 Console.Clear();
 
 Console.Write("Введите первое число: ");
@@ -12,15 +13,21 @@ int n2 = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int n3 = int.Parse(Console.ReadLine());
 
-if (n1 > n2 & n1 > n3)
-{
-    Console.WriteLine($"Max = {n1}");
-}
-if (n2 > n1 & n2 > n3)
-{
-    Console.WriteLine($"Max = {n2}");
-}
-if (n3 > n1 & n3 > n2)
-{
-    Console.WriteLine($"Max = {n3}");
-}
+        // if (n1 > n2 && n1 > n3)
+        // {
+        //     Console.WriteLine($"Max = {n1}");
+        // }
+        // else if (n2 > n1 && n2 > n3)
+        // {
+        //     Console.WriteLine($"Max = {n2}");
+        // }
+        // else (n3 > n1 && n3 > n2)
+        // {
+        //     Console.WriteLine($"Max = {n3}");
+        // }
+int max = n1;
+
+if (n2 > max) max = n2;
+else if (n3 > max) max = n3;
+
+Console.WriteLine($"Max = {max}");
